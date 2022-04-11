5f9de7601e2a97e95a7ced889b5e8518abc12c03
Jeroen <jjbozon@hotmail.nl>
2022-04-11 15:47:03 +0200

    Knoppen start, stop toegevoegd

--- Python Code/Sorteer_Systeem_Python.py
+++ Python Code/Sorteer_Systeem_Python.py
@@ -13,10 +13,10 @@ def openButtonWindow():
     # Toplevel widget
     buttonWindow.title("Positie")
 
-    buttonPosition0 = Button(buttonWindow, text="0", width=20, height=6)
-    buttonPosition1 = Button(buttonWindow, text="1", width=20, height=6)
-    buttonPosition2 = Button(buttonWindow, text="2", width=20, height=6)
-    buttonPosition3 = Button(buttonWindow, text="3", width=20, height=6)
+    buttonPosition0 = Button(buttonWindow, text="0", width=20, height=3)
+    buttonPosition1 = Button(buttonWindow, text="1", width=20, height=3)
+    buttonPosition2 = Button(buttonWindow, text="2", width=20, height=3)
+    buttonPosition3 = Button(buttonWindow, text="3", width=20, height=3)
 
     buttonFinished = Button(buttonWindow, text="Klaar", width=10, height=3, command=buttonWindow.destroy)
 
@@ -36,6 +36,9 @@ Grid.rowconfigure(windowMain, 4, weight=1)
 
 Grid.columnconfigure(windowMain, 0, weight=1)
 Grid.columnconfigure(windowMain, 1, weight=1)
+Grid.columnconfigure(windowMain, 2, weight=1)
+Grid.columnconfigure(windowMain, 3, weight=1)
+Grid.columnconfigure(windowMain, 4, weight=1)
 
 # frameRegister = Frame(windowMain)
 # frameSortingButtonModes = Frame(windowMain)
@@ -51,33 +54,37 @@ Grid.columnconfigure(windowMain, 1, weight=1)
 labelRegister = Label(text="Register:")
 listBoxRegister = Listbox()
 
+labelRegister.grid(row=0, column=2, columnspan=3)
+listBoxRegister.grid(row=1, column=2, columnspan=3, rowspan=2, sticky="nsew")
+
 ############################Sorteerknoppen componenten############################
 labelSortingButtons = Label(text="Sorteer modus:")
 buttonAuto = Button(text="Automatisch")
 buttonManual = Button(text="Handmatig", command=openButtonWindow)
+buttonStart = Button(text="Start", bg='green',fg='white')
+buttonStop = Button(text="Stop", bg="red", fg='white')
+
+labelSortingButtons.grid(row=0, column=0, columnspan=2)
+buttonAuto.grid(row=2, column=0, columnspan=1, sticky="nsew")
+buttonManual.grid(row=2, column=1, columnspan=1, sticky="nsew")
+buttonStart.grid(row=1, column=0, columnspan=1, sticky="nsew")
+buttonStop.grid(row=1, column=1, columnspan=1, sticky="nsew")
 
 ############################Wachtrij componenten############################
 labelWaitlist = Label(text="Wachtrij:")
 buttonWaitlist = Button(text="Wachtrij aanpassen")
 listBoxWaitlist = Listbox()
 
+labelWaitlist.grid(column=2, columnspan=2, row=3, rowspan=1)
+listBoxWaitlist.grid(column=2, columnspan=2, row=4, rowspan=1, sticky="nsew")
+buttonWaitlist.grid(column=4, row=4, sticky="nsew")
+
 ############################Meldingen componenten############################
 labelBoxMessages = Label(text="Meldingen:")
 listBoxMessages = Listbox()
 
-labelSortingButtons.grid(row=0, column=0)
-buttonAuto.grid(row=1, column=0, sticky="nsew")
-buttonManual.grid(row=2, column=0, sticky="nsew")
-
-labelRegister.grid(row=0, column=1, columnspan=2)
-listBoxRegister.grid(row=1, column=1, columnspan=2, rowspan=2, sticky="nsew")
-
-labelWaitlist.grid(column=1, columnspan=2, row=3, rowspan=1)
-buttonWaitlist.grid(column=2, columnspan=1, row=4, rowspan=1, sticky="nsew")
-listBoxWaitlist.grid(column=1, columnspan=2, row=4, rowspan=1, sticky="nsew")
-
-listBoxMessages.grid(column=0, row=4, sticky="nsew")
-labelBoxMessages.grid(column=0, row=3)
+labelBoxMessages.grid(column=0, row=3, columnspan=2, rowspan=1)
+listBoxMessages.grid(column=0, row=4, columnspan=2, rowspan=1, sticky="nsew")
 
 # for i in range(100):
 #     listBoxRegister.insert(i, "Nummer" + str(i))
