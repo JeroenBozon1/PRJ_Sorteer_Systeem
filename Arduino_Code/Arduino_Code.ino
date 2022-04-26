9c9488fa847234a7177b81f82d58d4345a9dd0c0
W.J. Nobel <1005174@hr.nl>
2022-04-26 12:56:26 +0200

    arduino code update 26-4

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -7,6 +7,14 @@ void setup() {
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(inductie, INPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(4, HIGH);
+
 }
 
 void loop() {
@@ -15,67 +23,74 @@ void loop() {
   // Aan de hand van seriele communcatie positie kiezen
   while (Serial.available() == 0) {}
   positie = Serial.parseInt();
-  Serial.println(positie);
+  //Serial.println(positie);
 
   if (positie == 1) {
-    hoek = 10;
+    hoek = 3;
     //potje oppakken
     DC_links();
     inductieSensor(hoek);
-    Serial.println("eerste hoek voltooid");
+    //Serial.println("eerste hoek voltooid");
     DC_stop();
-    delay(1000);
+    stepperRechts();
+    stepperLinks();
     //potje neerzetten
     DC_rechts();
     inductieSensor(hoek);
-    Serial.println("tweede hoek voltooid");
+    //Serial.println("tweede hoek voltooid");
     DC_stop();
   }
   else if (positie == 2) {
-    hoek = 15;
+    hoek = 3;
     //potje oppakken
     DC_rechts();
     inductieSensor(hoek);
-    DC_stop();
+    DC_stop();;
+    stepperRechts();
+    //stepperLinks();
     //potje neerzetten
     DC_links();
     inductieSensor(hoek);
     DC_stop();
   }
   else if (positie == 3) {
-    hoek = 20;
+    hoek = 9;
     //potje oppakken
     DC_rechts();
     inductieSensor(hoek);
     DC_stop();
+    stepperRechts();
+    //stepperLinks();
     //potje neerzetten
     DC_links();
     inductieSensor(hoek);
     DC_stop();
   }
   else if (positie == 4) {
-    hoek = 25;
+    hoek = 12;
     //potje oppakken
     DC_rechts();
     inductieSensor(hoek);
     DC_stop();
+    stepperRechts();
+    //stepperLinks();
     //potje neerzetten
     DC_links();
     inductieSensor(hoek);
     DC_stop();
   }
-  positie = 5;
+  positie = 0;
 }
 
 // DC motor links omdraaien
 void DC_links() {
   analogWrite(10, 0);
-  analogWrite(11, 150);
+  analogWrite(11, 255);
 }
 
 // DC motor rechts omdraaien
 void DC_rechts() {
-  analogWrite(10, 150);
+  analogWrite(10, 255);
   analogWrite(11, 0);
 }
 
@@ -91,19 +106,42 @@ int inductieSensor(int Hoek) {
   for (int x = 0; x < Hoek; x++) { //0 metaal, 1 lucht)
     while (digitalRead(inductie) == 0) {
     }
-    Serial.println(digitalRead(inductie));
+    //Serial.println(digitalRead(inductie));
 
     while (digitalRead(inductie) == 1) {
     }
-    Serial.println(digitalRead(inductie));
-          //Serial.println(x);
-
+    //Serial.println(digitalRead(inductie));
   }
 }
 
-// serial print variable type
-void types(String a) { Serial.println("it's a String"); }
-void types(int a) { Serial.println("it's an int"); }
-void types(char *a) { Serial.println("it's a char*"); }
-void types(float a) { Serial.println("it's a float"); }
-void types(bool a) { Serial.println("it's a bool"); }
+//stepper rechtsom
+void stepperRechts() {
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+
+  //while(analogRead("A1") > 200){
+  for (int i = 0; i < 2000; i++) {
+    digitalWrite(3, HIGH);
+    delay(1);
+    digitalWrite(3, LOW);
+    delay(1);
+  }
+  digitalWrite(4, HIGH);
+
+}
+
+//stepper linksom
+void stepperLinks() {
+  digitalWrite(2, HIGH);
+  digitalWrite(4, LOW);
+
+  //while(analogRead("A1") > 200){
+  for (int i = 0; i < 2000; i++) {
+    digitalWrite(3, HIGH);
+    delay(1);
+    digitalWrite(3, LOW);
+    delay(1);
+  }
+  digitalWrite(4, HIGH);
+
+}
